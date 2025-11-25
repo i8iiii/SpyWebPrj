79fc2ffca97b17d6b9b8d2bfe221f7f345ec24e9
FoiBem2131 <trunghuyh2@gmail.com>
2025-11-25 21:57:58 +0700

    Revised Buttons

--- client/assets/js/menu.js
+++ client/assets/js/menu.js
@@ -25,24 +25,36 @@ function getPingCmd() {
 }
 
 function getKeyLog() {
-  sendCommand("CMD_KEYLOG_START");
+  const command = document.getElementById("keylog-cmd");
+  if (command.dataset.state.trim().toLowerCase() === "off") {
+    // Keylog is OFF → ask to turn ON
+    sendCommand("PLACE HOLDER 1");
+    command.dataset.state = "on";
+    command.textContent = "Stop"; // show "Stop" because keylog is now ON
+  } else {
+    sendCommand("PLACE HOLDER 2");
+    command.dataset.state = "off";
+    command.textContent = "Start"; // show "Start" because keylog is now OFF
+  }
 }
 
 function toggleWebcam() {
   const command = document.getElementById("webcam-cmd");
 
   // Webcam is ON when the text says "Off"
-  const webcamIsOn = command.textContent.trim().toLowerCase() === "off";
+  const webcamIsOn = command.dataset.state.trim().toLowerCase() === "on";
 
   if (!webcamIsOn) {
     // Webcam is OFF → ask to turn ON
     if (confirm("CẢNH BÁO: Bạn có muốn mở camera?")) {
       sendCommand("CMD_CAM_ON");
+      command.dataset.state = "on";
       command.textContent = "Off"; // show "Off" because webcam is now ON
     }
   } else {
     // Webcam is ON → turn OFF
     sendCommand("CMD_CAM_OFF");
+    command.dataset.state = "off";
     command.textContent = "On"; // show "On" because webcam is now OFF
   }
 }
--- server/obj/Debug/net8.0-windows/server.AssemblyInfo.cs
+++ server/obj/Debug/net8.0-windows/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8dcbbaccc744b147073590cd7970f70728ef7901")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee863001c1b4f651d359219e8d419cf9b2e4f904")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
